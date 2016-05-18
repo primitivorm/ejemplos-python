a2803332d9156095e8154440925cee6c4de123b7
primitivorm <cibercafe_montero@hotmail.com>
2016-05-18 17:09:26 -0500

    update files

---
+++ list.py
@@ -0,0 +1,2 @@
+numeros = [1, 2, 3, 4, 5]
+print(numeros[1]);
--- test.py
+++ test.py
@@ -1,4 +1,11 @@
-x = 1
-y = 2
-z = x + y
-print z
+import dis
+
+def f(a, b):
+    return a + b
+
+dis.dis(f)
+f(1, 2)
+#x = 1
+#y = 2
+#z = x + y
+#print z
