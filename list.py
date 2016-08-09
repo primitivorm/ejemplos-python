2b9231e18071271046125212dc8a3a78d828a725
primitivorm <cibercafe_montero@hotmail.com>
2016-08-09 17:12:50 -0500

    update files

--- list.py
+++ list.py
@@ -1,3 +1,10 @@
-numeros = [1, 2, 3, 4, 5]
-numeros[0] = 10
-print(numeros[1]);
+#numeros = [1, 2, 3, 4, 5]
+#numeros[0] = 10
+#print(numeros[1]);
+
+lista = []
+i = 0
+while i < 1000000:
+    print str(i)
+    lista.append(i)
+    i = i + 1
